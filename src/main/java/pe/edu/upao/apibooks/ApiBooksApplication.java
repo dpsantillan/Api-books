1578f065021cf257b50b3a5b4e31018614c07e4d
dpsantillan <dsantillanp2@upao.edu.pe>
2023-04-29 18:47:45 -0500

    first commit

---
+++ src/main/java/pe/edu/upao/apibooks/ApiBooksApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.apibooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBooksApplication.class, args);
+    }
+
+}
